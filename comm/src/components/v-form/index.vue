067d840f29e24e30ab27713c92e63e20f7a78e92
zongmingzou <309663976@qq.com>
2021-08-11 15:24:53 +0800

    table 表单优化

--- comm/src/components/v-form/index.vue
+++ comm/src/components/v-form/index.vue
@@ -16,15 +16,19 @@ export default {
     }
   },
   mounted() {
-    window.addEventListener('resize', () => {
-      const elHeight = this.$refs.formGroup.offsetHeight || 0
-      this.elHeight = elHeight
-      if (elHeight <= 52) {
-        this.socureElHeight = 52
-      }
-    })
+    this.init()
   },
   methods: {
+    init() {
+      window.addEventListener('resize', () => {
+        const elHeight = this.$refs.formGroup.offsetHeight || 0
+        this.elHeight = elHeight
+        if (elHeight <= 52) {
+          this.socureElHeight = 52
+        }
+      })
+    },
+
     // 是否展开
     handleOpen() {
       this.isOpen = !this.isOpen
@@ -34,15 +38,18 @@ export default {
         this.socureElHeight = 52
       }
     },
+
     // 查询
     handleQuery() {
       this.$emit('query', this.form)
     },
+
     // 重置
     handleReset() {
       this.form = this.$options.data().form
       this.$emit('reset')
     },
+
     // formItem构建
     createFormItem(form) {
       return form.map((f) => {
@@ -80,16 +87,23 @@ export default {
 
   render() {
     return (
-      <div class="form-view-box" ref="formWarp" style={{ height: this.socureElHeight + 'px' }}>
+      <div class="form-view-box" style={{ height: this.socureElHeight + 'px' }}>
         <section class="form-view" ref="formGroup">
           {this.createFormItem(this.formList)}
         </section>
         <div class="from-view-button-group">
-          <el-button size="small" onClick={this.handleReset}>重置</el-button>
+          <el-button size="small" onClick={this.handleReset}>
+            重置
+          </el-button>
           <el-button type="primary" size="small" onClick={this.handleQuery}>
             查询
           </el-button>
-          {this.elHeight > 52 ? <span onClick={this.handleOpen}> 展开</span> : ''}
+          {this.elHeight > 52 && (
+            <span class="is-open" onClick={this.handleOpen}>
+              {this.isOpen ? '收起' : '展开'} 
+              <em class={this.isOpen ? 'el-icon-arrow-up' : 'el-icon-arrow-down'}></em>
+            </span>
+          )}
         </div>
       </div>
     )
